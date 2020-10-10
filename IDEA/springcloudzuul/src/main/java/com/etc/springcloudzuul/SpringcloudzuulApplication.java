3af146640ef387f791c5bb1683e2a9282e0e3b36
Vsuzie <554791032@qq.com>
2020-10-10 16:54:51 +0800

    test1

---
+++ IDEA/springcloudzuul/src/main/java/com/etc/springcloudzuul/SpringcloudzuulApplication.java
@@ -0,0 +1,17 @@
+package com.etc.springcloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringcloudzuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudzuulApplication.class, args);
+    }
+
+}
